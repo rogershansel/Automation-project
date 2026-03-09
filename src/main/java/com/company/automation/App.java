156e1b92c35ccd4291a346de32175b639ba5fc36
rogershansel <rogershansel000@gmail.com>
2026-03-09 13:54:33 +0530

    hello

--- src/main/java/com/company/automation/App.java
+++ src/main/java/com/company/automation/App.java
@@ -4,10 +4,8 @@ package com.company.automation;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
     }
 }
